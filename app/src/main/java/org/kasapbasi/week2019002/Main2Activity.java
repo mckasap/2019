a773da040b8fd7e9835d02a86d4e0c8814d681bd
mckasap <mckasap@gmail.com>
2019-02-15 15:55:02 +0300

    İlk gönderim 22

--- app/src/main/java/org/kasapbasi/week2019002/Main2Activity.java
+++ app/src/main/java/org/kasapbasi/week2019002/Main2Activity.java
@@ -2,12 +2,21 @@ package org.kasapbasi.week2019002;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        tv=(TextView) findViewById(R.id.textView);
+
+        tv.setText(getIntent().getStringExtra("metin"));
+
+
+
     }
 }
--- app/src/main/java/org/kasapbasi/week2019002/MainActivity.java
+++ app/src/main/java/org/kasapbasi/week2019002/MainActivity.java
@@ -5,17 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btgit;
+    private EditText et;
 
     class mylistener implements  View.OnClickListener{
         @Override
         public void onClick(View view) {
 
             Intent myInt = new Intent(MainActivity.this,Main2Activity.class);
+
+            myInt.putExtra("metin", et.getText().toString());
 
             startActivity(myInt);
             Toast.makeText(MainActivity.this," HELLLO",Toast.LENGTH_LONG).show();
@@ -30,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btgit= (Button) findViewById(R.id.btnGo);
-
+        et=(EditText) findViewById(R.id.editText);
 
         btgit.setOnClickListener(new mylistener());
     }
